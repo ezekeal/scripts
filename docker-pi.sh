6e4d64137f02eda401b2ec4964dabc7d10f88e11
Zach Koch <6458318+ezekeal@users.noreply.github.com>
2024-09-05 17:31:40 -0500

    auto confirm apt-get

--- docker-pi.sh
+++ docker-pi.sh
@@ -18,4 +18,4 @@ echo \
 sudo apt-get update
 
 echo "Install Docker packages"
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
