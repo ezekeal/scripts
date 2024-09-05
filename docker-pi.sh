164c9a089cb90bc7845c9b48b6dd3ee07132addb
Zach Koch <6458318+ezekeal@users.noreply.github.com>
2024-09-05 17:37:07 -0500

    add test

--- docker-pi.sh
+++ docker-pi.sh
@@ -15,7 +15,10 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get -y update
 
 echo "Install Docker packages"
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "Testing Installation"
+sudo docker run hello-world
